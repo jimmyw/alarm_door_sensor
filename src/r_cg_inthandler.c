be2197e68bd9df11cab1acbe0a0f7edbfc18e009
Jimmy Wennlund <jimmy@wennlund.nu>
2026-05-10 15:15:06 +0200

    Blinking led

--- src/r_cg_inthandler.c
+++ src/r_cg_inthandler.c
@@ -1,30 +1,34 @@
 /***********************************************************************************************************************
-* DISCLAIMER
-* This software is supplied by Renesas Electronics Corporation and is only intended for use with Renesas products.
-* No other uses are authorized. This software is owned by Renesas Electronics Corporation and is protected under all
-* applicable laws, including copyright laws. 
-* THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING THIS SOFTWARE, WHETHER EXPRESS, IMPLIED
-* OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-* NON-INFRINGEMENT.  ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY
-* LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE FOR ANY DIRECT,
-* INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR
-* ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability 
-* of this software. By using this software, you agree to the additional terms and conditions found by accessing the 
-* following link:
-* http://www.renesas.com/disclaimer
-*
-* Copyright (C) 2024, 2024 Renesas Electronics Corporation. All rights reserved.
-***********************************************************************************************************************/
+ * DISCLAIMER
+ * This software is supplied by Renesas Electronics Corporation and is only
+ * intended for use with Renesas products. No other uses are authorized. This
+ * software is owned by Renesas Electronics Corporation and is protected under
+ * all applicable laws, including copyright laws. THIS SOFTWARE IS PROVIDED "AS
+ * IS" AND RENESAS MAKES NO WARRANTIES REGARDING THIS SOFTWARE, WHETHER EXPRESS,
+ * IMPLIED OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.  ALL
+ * SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.TO THE MAXIMUM EXTENT PERMITTED NOT
+ * PROHIBITED BY LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS
+ * AFFILIATED COMPANIES SHALL BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL,
+ * INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO THIS SOFTWARE,
+ * EVEN IF RENESAS OR ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGES. Renesas reserves the right, without notice, to make changes to
+ * this software and to discontinue the availability of this software. By using
+ * this software, you agree to the additional terms and conditions found by
+ * accessing the following link: http://www.renesas.com/disclaimer
+ *
+ * Copyright (C) 2024, 2024 Renesas Electronics Corporation. All rights
+ * reserved.
+ ***********************************************************************************************************************/
 
 /***********************************************************************************************************************
-* File Name    : r_cg_inthandler.c
-* Version      : Code Generator for RL78/G12 V1.0.0 [01 Jul 2024]
-* Device(s)    : R5F1026A
-* Tool-Chain   : LLVMRL78
-* Description  : This file declares interrupt service routine.
-* Creation Date: 2026/05/10
-***********************************************************************************************************************/
+ * File Name    : r_cg_inthandler.c
+ * Version      : Code Generator for RL78/G12 V1.0.0 [01 Jul 2024]
+ * Device(s)    : R5F1026A
+ * Tool-Chain   : LLVMRL78
+ * Description  : This file declares interrupt service routine.
+ * Creation Date: 2026/05/10
+ ***********************************************************************************************************************/
 
 /***********************************************************************************************************************
 Includes
@@ -39,121 +43,121 @@ Global variables and functions
 /*
  * INT_WDTI (0x4)
  */
-void INT_WDTI (void) { }
+void INT_WDTI(void) {}
 
 /*
  * INT_LVI (0x6)
  */
-void INT_LVI (void) { }
+void INT_LVI(void) {}
 
 /*
  * INT_P0 (0x8)
  */
-void INT_P0 (void) { }
+void INT_P0(void) {}
 
 /*
  * INT_P1 (0xA)
  */
-void INT_P1 (void) { }
+void INT_P1(void) {}
 
 /*
  * INT_P2 (0xC)
  */
-void INT_P2 (void) { }
+void INT_P2(void) {}
 
 /*
  * INT_P3 (0xE)
  */
-void INT_P3 (void) { }
+void INT_P3(void) {}
 
 /*
  * INT_DMA0 (0x10)
  */
-void INT_DMA0 (void) { }
+void INT_DMA0(void) {}
 
 /*
  * INT_DMA1 (0x12)
  */
-void INT_DMA1 (void) { }
+void INT_DMA1(void) {}
 
 /*
  * INT_ST0/INT_CSI00/INT_IIC00 (0x14)
  */
-void INT_ST0 (void) { }
-//void INT_CSI00 (void) { }
-//void INT_IIC00 (void) { }
+void INT_ST0(void) {}
+// void INT_CSI00 (void) { }
+// void INT_IIC00 (void) { }
 
 /*
  * INT_SR0/INT_CSI01/INT_IIC01 (0x16)
  */
-void INT_SR0 (void) { }
-//void INT_CSI01 (void) { }
-//void INT_IIC01 (void) { }
+void INT_SR0(void) {}
+// void INT_CSI01 (void) { }
+// void INT_IIC01 (void) { }
 
 /*
  * INT_SRE0 (0x18)
  */
-void INT_SRE0 (void) { }
+void INT_SRE0(void) {}
 
 /*
  * INT_TM01H (0x1A)
  */
-void INT_TM01H (void) { }
+void INT_TM01H(void) {}
 
 /*
  * INT_TM03H (0x1C)
  */
-void INT_TM03H (void) { }
+void INT_TM03H(void) {}
 
 /*
  * INT_IICA0 (0x1E)
  */
-void INT_IICA0 (void) { }
+void INT_IICA0(void) {}
 
 /*
  * INT_TM00 (0x20)
  */
-void INT_TM00 (void) { }
+void INT_TM00(void) {}
 
 /*
  * INT_TM01 (0x22)
  */
-void INT_TM01 (void) { }
+void INT_TM01(void) {}
 
 /*
  * INT_TM02 (0x24)
  */
-void INT_TM02 (void) { }
+void INT_TM02(void) {}
 
 /*
  * INT_TM03 (0x26)
  */
-void INT_TM03 (void) { }
+void INT_TM03(void) {}
 
 /*
  * INT_AD (0x28)
  */
-void INT_AD (void) { }
+void INT_AD(void) {}
 
 /*
  * INT_IT (0x2A)
  */
-void INT_IT (void) { }
+// void INT_IT (void) { }
 
 /*
  * INT_KR (0x2C)
  */
-void INT_KR (void) { }
+void INT_KR(void) {}
 
 /*
  * INT_MD (0x2E)
  */
-void INT_MD (void) { }
+void INT_MD(void) {}
 
 /*
  * INT_BRK (0x7E)
  */
-void INT_BRK (void) { }
+void INT_BRK(void) {}
 
 /* Start user code for adding. Do not edit comment generated here */
 /* End user code. Do not edit comment generated here */
--- src/r_main.c
+++ src/r_main.c
@@ -1,36 +1,41 @@
 /***********************************************************************************************************************
-* DISCLAIMER
-* This software is supplied by Renesas Electronics Corporation and is only intended for use with Renesas products.
-* No other uses are authorized. This software is owned by Renesas Electronics Corporation and is protected under all
-* applicable laws, including copyright laws. 
-* THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING THIS SOFTWARE, WHETHER EXPRESS, IMPLIED
-* OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-* NON-INFRINGEMENT.  ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY
-* LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE FOR ANY DIRECT,
-* INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR
-* ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability 
-* of this software. By using this software, you agree to the additional terms and conditions found by accessing the 
-* following link:
-* http://www.renesas.com/disclaimer
-*
-* Copyright (C) 2024, 2024 Renesas Electronics Corporation. All rights reserved.
-***********************************************************************************************************************/
+ * DISCLAIMER
+ * This software is supplied by Renesas Electronics Corporation and is only
+ * intended for use with Renesas products. No other uses are authorized. This
+ * software is owned by Renesas Electronics Corporation and is protected under
+ * all applicable laws, including copyright laws. THIS SOFTWARE IS PROVIDED "AS
+ * IS" AND RENESAS MAKES NO WARRANTIES REGARDING THIS SOFTWARE, WHETHER EXPRESS,
+ * IMPLIED OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.  ALL
+ * SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.TO THE MAXIMUM EXTENT PERMITTED NOT
+ * PROHIBITED BY LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS
+ * AFFILIATED COMPANIES SHALL BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL,
+ * INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO THIS SOFTWARE,
+ * EVEN IF RENESAS OR ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGES. Renesas reserves the right, without notice, to make changes to
+ * this software and to discontinue the availability of this software. By using
+ * this software, you agree to the additional terms and conditions found by
+ * accessing the following link: http://www.renesas.com/disclaimer
+ *
+ * Copyright (C) 2024, 2024 Renesas Electronics Corporation. All rights
+ * reserved.
+ ***********************************************************************************************************************/
 
 /***********************************************************************************************************************
-* File Name    : r_main.c
-* Version      : Code Generator for RL78/G12 V1.0.0 [01 Jul 2024]
-* Device(s)    : R5F1026A
-* Tool-Chain   : LLVMRL78
-* Description  : This file implements main function.
-* Creation Date: 2026/05/10
-***********************************************************************************************************************/
+ * File Name    : r_main.c
+ * Version      : Code Generator for RL78/G12 V1.0.0 [01 Jul 2024]
+ * Device(s)    : R5F1026A
+ * Tool-Chain   : LLVMRL78
+ * Description  : This file implements main function.
+ * Creation Date: 2026/05/10
+ ***********************************************************************************************************************/
 
 /***********************************************************************************************************************
 Includes
 ***********************************************************************************************************************/
-#include "r_cg_macrodriver.h"
+#include "interrupt_handlers.h"
 #include "r_cg_cgc.h"
+#include "r_cg_macrodriver.h"
 /* Start user code for include. Do not edit comment generated here */
 /* End user code. Do not edit comment generated here */
 #include "r_cg_userdefine.h"
@@ -43,37 +48,53 @@ Global variables and functions
 void R_MAIN_UserInit(void);
 
 /***********************************************************************************************************************
-* Function Name: main
-* Description  : This function implements main function.
-* Arguments    : None
-* Return Value : None
-***********************************************************************************************************************/
-int main(void)
-{
-    R_MAIN_UserInit();
-    /* Start user code for here. Do not edit comment generated here */
-    /* End user code. Do not edit comment generated here */
-    return 0U;
+ * Function Name: main
+ * Description  : This function implements main function.
+ * Arguments    : None
+ * Return Value : None
+ ***********************************************************************************************************************/
+int main(void) {
+  R_MAIN_UserInit();
+  // while (1) {
+  //  STOP(); // deep sleep, wakes on IT interrupt
+  //}
+  return 0;
 }
 
-
 /***********************************************************************************************************************
-* Function Name: R_MAIN_UserInit
-* Description  : This function adds user code before implementing main function.
-* Arguments    : None
-* Return Value : None
-***********************************************************************************************************************/
-void R_MAIN_UserInit(void)
-{
-    EI();
+ * Function Name: R_MAIN_UserInit
+ * Description  : This function adds user code before implementing main
+ * function. Arguments    : None Return Value : None
+ ***********************************************************************************************************************/
+void R_MAIN_UserInit(void) {
+  EI();
 
-    ADPC = 0x01U; // Turn all outputs into Digital
+  ADPC = 0x01U; // Turn all outputs into Digital
 
-    P2  = 0x00;
-    PM2 = 0x00;  // all output
+  P2 = 0xff;
+  PM2 = 0x00; // all output
 
+  ///* Allow LOCO (low-speed oscillator) to keep running in STOP mode */
+  OSMC = _10_CGC_IT_CLK_FIL; // WUTMMCK0=1: IT runs on LOCO during STOP
+  //
+  ///* Enable clock to interval timer */
+  TMKAEN = 1U; // PER0 bit5 — TAU-KA clock on
+  //
+  ///* Configure Interval Timer for 500ms using LOCO @ 15kHz */make -C
+  TMKAMK = 1U;            // mask IT interrupt while configuring
+  TMKAIF = 0U;            // clear any pending flag
+  ITMC = 0x8000U | 7499U; // bit15=enable, lower 15 bits = compare value
+  TMKAMK = 0U;            // unmask — ready to fire
+  P2 = 0x00;              // turn on LED on P2.2
+}
 
-
+/* IT interrupt handler — fires every 500ms */
+void INT_IT(void) {
+  if (P2 & 0x1) {
+    P2 = 0x00; // turn on LED on P2.0
+  } else {
+    P2 = 0xff; // turn off LED on P2.0
+  }
 }
 
 /* Start user code for adding. Do not edit comment generated here */
